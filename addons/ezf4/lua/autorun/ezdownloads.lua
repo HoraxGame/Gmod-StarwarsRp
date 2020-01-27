117cc15ee8360bfc1b0c8a4164c1180dc614bec5
Thyvene <karim.el-karroumy@epitech.eu>
2020-01-27 23:12:34 +0100

    Core/Lua: Adding addons directory

---
+++ addons/ezf4/lua/autorun/ezdownloads.lua
@@ -0,0 +1,11 @@
+if SERVER then
+	local FastDL = false
+
+	if FastDL == true then
+		resource.AddFile( "materials/ezf4/closemenu.png" )
+		resource.AddFile( "materials/ezf4/dots.png" )
+		resource.AddFile( "materials/ezf4/ezf4lines.png" )
+	else
+		resource.AddWorkshop( "1210487641" )
+	end
+end
